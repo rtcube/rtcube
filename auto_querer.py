4a717287737778d489341bebd7a1b135d4a914b0
Paweł Kobojek <PawelKobojek@gmail.com>
2016-04-25 07:30:47 +0200

    automatic query launcher for experiment

---
+++ auto_querer.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.path.append("../")
+import rtquery as rtq
+import time
+
+
+addresses = [
+    ("::1", 2020)
+]
+
+# TODO
+"""
+Here comes list of addresses (most likely plain IPv4) of gpunodes in 304
+"""
+
+cubedef = open("cube").read()
+
+query = "SELECT MAX(m1), MAX(m2)"
+
+current_time_ms = lambda: int(round(time.time() * 1000))
+
+while True:
+    start_time = current_time_ms()
+    res = rtq.query(addresses, cubedef, query)
+    query_time = current_time_ms() - start_time
+
+    # TODO - ms?
+    print("result:", res[-1], "time[ms]:", query_time)
