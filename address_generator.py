52ecc80bbe858d414d6179e6055e856ca9dec98f
dlusm <dlusm@student.mini.pw.edu.pl>
2016-04-27 20:31:29 +0200

    addresses generator

---
+++ address_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Program(object):	
+	def main(self):
+		self.infilename = "addresses304"
+		self.tcpfilename = self.infilename + "_tcp"
+		self.udpfilename = self.infilename + "_udp"
+		with open(self.tcpfilename, "w") as tcpfile:
+			with open(self.udpfilename, "w") as udpfile:
+				for line in open(self.infilename, "r"):
+					print(line.strip('\n') + ":50000", file = tcpfile)
+					print(line.strip('\n') + ":50001", file = udpfile)
+
+if __name__ == "__main__":
+	Program().main()
