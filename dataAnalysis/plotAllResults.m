f33e5adce61ab6594b7ec218fca910a708511873
Albert Wolant <albertwolant@gmail.com>
2016-04-30 12:26:07 +0200

    Plots prepared

--- dataAnalysis/plotAllResults.m
+++ dataAnalysis/plotAllResults.m
@@ -1,10 +1,33 @@
-plot_results('results_4.txt');
-plot_results('results_8.txt');
-plot_results('results_12.txt');
-plot_results('results_16.txt');
-plot_results('results_20.txt');
-plot_results('results_24.txt');
-plot_results('results_28.txt');
-plot_results('results_32.txt');
-plot_results('results_36.txt');
-plot_results('results_40.txt');
+
+clear;
+X(1,:) = plot_results('results4.txt', 4);
+X(2,:) = plot_results('results8.txt', 8);
+X(3,:) = plot_results('results12.txt', 12);
+X(4,:) = plot_results('results16.txt', 16);
+X(5,:) = plot_results('results20.txt', 20);
+X(6,:) = plot_results('results24.txt', 24);
+X(7,:) = plot_results('results28.txt', 28);
+X(8,:) = plot_results('results32.txt', 32);
+X(9,:) = plot_results('results36.txt', 36);
+X(10,:) = plot_results('results40.txt', 40);
+
+figure;
+plot(X(:, 1), X(:, 3));
+title('Slope');
+
+figure;
+plot(X(:, 1), X(:, 2));
+title('Mean');
+
+figure;
+plot(X(:, 1), X(:, 4));
+title('Max');
+
+figure;
+plot(X(:, 1), X(:, 5));
+title('STD');
+
+figure;
+plot(X(:, 1), X(:, 6));
+title('Median');
+
--- dataAnalysis/plot_results.m
+++ dataAnalysis/plot_results.m
@@ -1,8 +1,12 @@
-function plot_results(filename)
+function X = plot_results(filename, nodes)
+
 	X = csvread(filename);
 	X = X(100:end, :);
-	figure()
-	plot(X(:, 1), X(:, 2) - X(:, 3));
-	disp(filename);
+    y = (X(:, 2) - X(:, 3) + abs(min( X(:, 2) - X(:, 3))));
+    
+	figure;
+	plot(X(:, 1), y);
 	title(filename);
+    
+    X = [nodes, mean(y), X(:,1)\y, max(y), std(y), median(y)];
 end
