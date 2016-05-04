a47f45f5c890d8a8bc9b0256b8bfcd5582cff9d3
Albert Wolant <albertwolant@gmail.com>
2016-05-04 11:58:47 +0200

    Plots RC

--- dataAnalysis/plotAllResults.m
+++ dataAnalysis/plotAllResults.m
@@ -1,13 +1,21 @@
 
 clear;
-X(1,:) = plot_results('results4.txt', 4, 2, 4, 1);
-X(2,:) = plot_results('results8.txt', 8, 2, 4, 2);
-X(3,:) = plot_results('results12.txt', 12, 2, 4, 3);
-X(4,:) = plot_results('results16.txt', 16, 2, 4, 4);
-X(5,:) = plot_results('results20.txt', 20, 2, 4, 5);
-X(6,:) = plot_results('results24.txt', 24, 2, 4, 6);
-X(7,:) = plot_results('results28.txt', 28, 2, 4, 7);
-X(8,:) = plot_results('results32.txt', 32, 2, 4, 8);
+% X(1,:) = plot_results('results4.txt', 4, 2, 4, 1);
+% X(2,:) = plot_results('results8.txt', 8, 2, 4, 2);
+% X(3,:) = plot_results('results12.txt', 12, 2, 4, 3);
+% X(4,:) = plot_results('results16.txt', 16, 2, 4, 4);
+% X(5,:) = plot_results('results20.txt', 20, 2, 4, 5);
+% X(6,:) = plot_results('results24.txt', 24, 2, 4, 6);
+% X(7,:) = plot_results('results28.txt', 28, 2, 4, 7);
+% X(8,:) = plot_results('results32.txt', 32, 2, 4, 8);
+
+X(1,:) = plot_results('results4.txt', 4, 2, 2, 4);
+X(2,:) = plot_results('results8.txt', 8, 2, 2, 1);
+X(3,:) = plot_results('results16.txt', 16, 2, 2, 2);
+X(4,:) = plot_results('results20.txt', 20, 2, 2, 4);
+X(5,:) = plot_results('results24.txt', 24, 2, 2, 3);
+X(6,:) = plot_results('results28.txt', 28, 2, 2, 4);
+X(7,:) = plot_results('results32.txt', 32, 2, 2, 4);
 
 % figure;
 % X(9,:) = plot_results('results36.txt', 36, 1, 2, 1);
@@ -48,7 +56,7 @@ plot(X(:, 1), X(:, 7));
 title('Variance');
 xlabel('Number of nodes');
 
-figure;
-X(9,:) = plot_results('results36.txt', 36, 1, 2, 1);
-X(10,:) = plot_results('results40.txt', 40, 1, 2, 2);
+% figure;
+% X(9,:) = plot_results('results36.txt', 36, 1, 2, 1);
+% X(10,:) = plot_results('results40.txt', 40, 1, 2, 2);
 
--- dataAnalysis/plot_results.m
+++ dataAnalysis/plot_results.m
@@ -2,13 +2,17 @@ function X = plot_results(filename, nodes, m, n, p)
 
 	X = csvread(filename);
 	X = X(100:end, :);
-    y = (X(:, 2) - X(:, 3) + abs(min( X(:, 2) - X(:, 3))));
+    
+    %y = (X(:, 2) - X(:, 3) + abs(min( X(:, 2) - X(:, 3))));
+    %600 = max[ abs(min( X(:, 2) - X(:, 3))) ] po wszystkich eksperymentach
+    
+    y = (X(:, 2) - X(:, 3) + 600);
     
     ax = subplot(m, n, p);
 	plot(X(:, 1), y);
 	title(['Number of nodes = ' num2str(nodes)]);
     
-    ylim(ax, [0 1000]);
+    ylim(ax, [0 1200]);
     xlim(ax, [100 500]);
     
     xlabel('Iteration');
