80fa0f1c902d0848c93ad2efb473f8da39518e87
Paweł Kobojek <PawelKobojek@gmail.com>
2016-05-08 18:18:17 +0200

    New experiment setup

--- experiment.sh
+++ experiment.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
-if [ $# -lt 2 ]
+if [ $# -lt 4 ]
 then
   echo "Error, missing arguments"
-  echo "USAGE: experiment.sh <addresses_for_querer> <addresses_for_generator>"
+  echo "USAGE: experiment.sh <addresses_for_querer> <addresses_for_generator> <querer_output> <generator_output>"
   exit 1
 fi
 
-python3 auto_querer.py $1 experiment/cudebef && bin/data-generator $2
+python3 auto_querer.py $1 experiment/cudebef > $3 &
+QPID=$!
+bin/data-generator $2 > $4 &
+GPID=$!
+
+trap 'kill $QPID ; kill $GPID' SIGINT
+wait
