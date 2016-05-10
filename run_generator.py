26c79fbbfbd3affbadba232f5d072286441be024
sealionkat <sealionkat@gmail.com>
2016-05-10 18:31:44 +0200

    generator with step

--- run_generator.py
+++ run_generator.py
@@ -10,20 +10,22 @@ class Program(object):
 		self.args = self.parse_args(args)
 		print(self.args)
 		self.processes = []
-		for id in range(self.args["id"], self.args["id"] + self.args["count"]):
+		for id in range(self.args["id"], self.args["id"] + self.args["count"] * self.args["step"], self.args["step"]):
 			self.run(id)
 		self.wait_all()
 
 	def parse_args(self, args):
-		result = { "count": 4, "addresses": "addresses_all_udp", "cubedef":"experiment/cubedef" }
+		result = { "count": 4, "addresses": "addresses_all_udp", "cubedef":"experiment/cubedef", "step": 1 }
 		if len(args) > 1:
 			result["id"] = int(args[1])
 		if len(args) > 2:
-			result["count"] = int(args[2])
+			result["step"] = int(args[2])
 		if len(args) > 3:
-			result["addresses"] = args[3]
+			result["count"] = int(args[3])
 		if len(args) > 4:
-			result["cubedef"] = args[4]
+			result["addresses"] = args[4]
+		if len(args) > 5:
+			result["cubedef"] = args[5]
 		return result
 
 	def run(self, id):
@@ -49,7 +51,7 @@ class Program(object):
 				p.send_signal(signal.SIGINT)
 	
 	def usage(self):
-		print("USAGE: run_generator.py START_ID [COUNT] [ADDRESSES_PATH] [CUBEDEF]")
+		print("USAGE: run_generator.py START_ID [ID_STEP = 1] [COUNT = 4] [ADDRESSES_PATH = addresses_all_udp] [CUBEDEF = experiment/cubedef]")
 	
 if __name__ == "__main__":
 	sys.exit(Program().main(sys.argv))
