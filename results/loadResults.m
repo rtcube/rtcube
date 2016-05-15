26ebe673bdd0f4ffa950dd55da8f13bec0410210
Albert Wolant <albertwolant@gmail.com>
2016-05-16 01:56:58 +0200

    Query speed plot

--- results/loadResults.m
+++ results/loadResults.m
@@ -1,10 +1,23 @@
-function [ A, x, y ] = loadResults( quererFile, generatorFile, nodes )
+function [ A, x, y, Z, C ] = loadResults( quererFile, generatorFile, nodes )
 
 Q = csvread(quererFile);
 G = csvread(generatorFile);
 
 X = ones(600, 3);
 Y = G(2:601,:);
+
+Z = ones(size(Q,1)-1,2);
+C = ones(size(Q,1),2);
+
+for i = 1:size(Q,1)
+    C(i,1) = Q(i,3)*10^-3;
+    C(i,2) = Q(i,2);
+end
+
+for i = 1:size(Q,1)-1
+    Z(i,1) = Q(i+1,3)*10^-3;
+    Z(i,2) = (Q(i+1,2)*16*10^-9)/((Q(i+1,3)-Q(i,3))*10^-3);
+end
 
 for i = size(Q, 1):-1:1
     if Q(i,1) == 0
--- results/printAllResults.m
+++ results/printAllResults.m
@@ -6,13 +6,13 @@ clear;
 % [X(3,:) x4 y4] = loadResults('8_1_querer.txt', '8_1_generator.txt', 8);
 % [X(4,:) x5 y5] = loadResults('10_1_querer.txt', '10_1_generator.txt', 10);
 
-[X(1,:) x1 y1] = loadResults('2_1_querer.txt', '2_1_generator.txt', 2);
-[X(2,:) x2 y2] = loadResults('4_1_querer.txt', '4_1_generator.txt', 4);
-[X(3,:) x3 y3] = loadResults('6_1_querer.txt', '6_1_generator.txt', 6);
-[X(4,:) x4 y4] = loadResults('8_1_querer.txt', '8_1_generator.txt', 8);
-[X(5,:) x5 y5] = loadResults('10_1_querer.txt', '10_1_generator.txt', 10);
-[X(6,:) x6 y6] = loadResults('12_1_querer.txt', '12_1_generator.txt', 12);
-[X(7,:) x7 y7] = loadResults('14_1_querer.txt', '14_1_generator.txt', 14);
+[X(1,:) x1 y1 Z1 C1] = loadResults('2_1_querer.txt', '2_1_generator.txt', 2);
+[X(2,:) x2 y2 Z2 C2] = loadResults('4_1_querer.txt', '4_1_generator.txt', 4);
+[X(3,:) x3 y3 Z3 C3] = loadResults('6_1_querer.txt', '6_1_generator.txt', 6);
+[X(4,:) x4 y4 Z4 C4] = loadResults('8_1_querer.txt', '8_1_generator.txt', 8);
+[X(5,:) x5 y5 Z5 C5] = loadResults('10_1_querer.txt', '10_1_generator.txt', 10);
+[X(6,:) x6 y6 Z6 C6] = loadResults('12_1_querer.txt', '12_1_generator.txt', 12);
+[X(7,:) x7 y7 Z7 C7] = loadResults('14_1_querer.txt', '14_1_generator.txt', 14);
 
 %Ploting results from experiments
 figure;
@@ -32,6 +32,32 @@ plotPoints(x6, y6);
 plotPoints(x7, y7);
 
 hold off;
+
+figure
+hold on;
+scatter(Z1(:,1), Z1(:,2), 'filled');
+scatter(Z2(:,1), Z2(:,2), 'filled');
+scatter(Z3(:,1), Z3(:,2), 'filled');
+scatter(Z4(:,1), Z4(:,2), 'filled');
+scatter(Z5(:,1), Z5(:,2), 'filled');
+scatter(Z6(:,1), Z6(:,2), 'filled');
+scatter(Z7(:,1), Z7(:,2), 'filled');
+hold off;
+xlabel('Time [seconds]');
+ylabel('Speed [Gigabytes per second]');
+
+figure
+hold on;
+scatter(C1(:,1), C1(:,2), 'filled');
+scatter(C2(:,1), C2(:,2), 'filled');
+scatter(C3(:,1), C3(:,2), 'filled');
+scatter(C4(:,1), C4(:,2), 'filled');
+scatter(C5(:,1), C5(:,2), 'filled');
+scatter(C6(:,1), C6(:,2), 'filled');
+scatter(C7(:,1), C7(:,2), 'filled');
+hold off;
+xlabel('Time [seconds]');
+ylabel('Count [rows]');
 
 figure;
 subplot(1, 2, 1);
